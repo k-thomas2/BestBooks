a9a3307bd428d609b7be4d8b478828205a4f4201
Kate Thomas <kpthomas6@hotmail.com>
2018-11-29 20:36:59 -0500

    added class

---
+++ BestBooks.Data/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBooks.Data
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
