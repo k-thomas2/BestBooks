eea9921c33389a8f9099acd221dee1a0d9713d80
Kate Thomas <kpthomas6@hotmail.com>
2018-11-29 20:19:50 -0500

    update

--- BestBooks.Data/IdentityModels.cs
+++ BestBooks.Data/IdentityModels.cs
@@ -1,8 +1,10 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace BestBooks.Data
 {
@@ -16,18 +18,50 @@ namespace BestBooks.Data
             // Add custom user claims here
             return userIdentity;
         }
+        public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+        {
+            public ApplicationDbContext()
+                : base("DefaultConnection", throwIfV1Schema: false)
+            {
+            }
+            public static ApplicationDbContext Create()
+            {
+                return new ApplicationDbContext();
+            }
+
+            public DbSet<Note> Notes { get; set; }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+
+                modelBuilder
+                    .Conventions
+                    .Remove<PluralizingTableNameConvention>();
+                modelBuilder
+                    .Configurations
+                    .Add(new IdentityUserLoginConfiguration())
+                    .Add(new IdentityUserRoleConfiguration());
+            }
+
+            public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+            {
+            public IdentityUserLoginConfiguration()
+                {
+                    HasKey(iul => iul.UserId);
+                }
+            }
+
+            public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+            {
+                public IdentityUserRoleConfiguration()
+                {
+                    HasKey(iur => iur.RoleId);
+                }
+            }
+
+        }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
 
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+
 }
