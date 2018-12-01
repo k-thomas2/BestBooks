1363bb08b87400651870bb1caba8c9f9b2d02478
Kate Thomas <kpthomas6@hotmail.com>
2018-11-30 21:53:36 -0500

    refactored index

---
+++ BestBooks_MVC/Models/BookListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestBooks_MVC.Models
+{
+    public class BookListItem
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
