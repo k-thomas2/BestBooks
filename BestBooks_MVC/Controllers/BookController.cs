699eae4171d06c55f3931c757b1dd7cdfeccc8f2
Kate Thomas <kpthomas6@hotmail.com>
2018-12-02 20:37:48 -0500

    added class

--- BestBooks_MVC/Controllers/BookController.cs
+++ BestBooks_MVC/Controllers/BookController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BestBooks_MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,10 +7,18 @@ using System.Web.Mvc;
 
 namespace BestBooks_MVC.Controllers
 {
+    [Authorize]
     public class BookController : Controller
     {
         // GET: Book
         public ActionResult Index()
+        {
+            var model = new BookListItem[0];
+            return View(model);
+        }
+
+        //Get
+        public ActionResult Create()
         {
             return View();
         }
