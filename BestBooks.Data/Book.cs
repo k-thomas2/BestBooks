ea2ea747ade6e41630ae0293a702a8e12860c148
Kate Thomas <kpthomas6@hotmail.com>
2018-12-03 13:29:10 -0500

    bug fixes

--- BestBooks.Data/Book.cs
+++ BestBooks.Data/Book.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BestBooks.Data
 {
     public class Book
     {
-        List<Book> _books = new List<Book>();
-       public enum Genre { scienceFiction, satire, drama, action, romance, mystery, horror, selfHelp, health, guide, childrens, spirituality, science, history, fantasy, comic, journal, series, biography, autobiography, poetry}
         [Key]
         public int BookId { get; set; }
+        [Required]
+        List<Book> _books = new List<Book>();
+        public enum Genre { scienceFiction, satire, drama, action, romance, mystery, horror, selfHelp, health, guide, childrens, spirituality, science, history, fantasy, comic, journal, series, biography, autobiography, poetry }
+        public Guid OwnerId { get; set; }
         [Required]
         public string Title { get; set; }
         [Required]
@@ -20,8 +19,8 @@ namespace BestBooks.Data
         [Required]
         public string Description { get; set; }
         [Required]
-        public  Genre BookGenre { get; set; }
-        [Key]
+        public Genre BookGenre { get; set; }
+        [Required]
         public int ReviewId { get; set; }
         public decimal AvgBookRating { get; set; }
     }
