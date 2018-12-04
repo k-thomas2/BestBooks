38ea051c7d9df12c9f06bda4ffc439cfb031909d
Kate Thomas <kpthomas6@hotmail.com>
2018-12-04 10:21:38 -0500

    review views

--- BestBooks.Data/Review.cs
+++ BestBooks.Data/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,21 @@ namespace BestBooks.Data
 {
    public class Review
     {
+        [Key]
         public int ReviewId { get; set; }
         public Guid OwnderId { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Summary { get; set; }
+        [Required]
         public string ReviewContent { get; set; }
         public int BookId { get; set; }
         public virtual Book Book { get; set; }
         public decimal AvgBookRating { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
