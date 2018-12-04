a510facb256ef6663253ac585ad180ed91520b96
Kate Thomas <kpthomas6@hotmail.com>
2018-12-04 11:08:55 -0500

    create review method

---
+++ BestBooks.Services/ReviewService.cs
@@ -0,0 +1,40 @@
+﻿using BestBooks.Data;
+using BestBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BestBooks.Data.ApplicationUser;
+
+namespace BestBooks.Services
+{
+    public class ReviewService
+    {
+        private readonly Guid _userId;
+
+        public ReviewService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        //GET:Create
+        public bool CreateReview(ReviewCreate model)
+        {
+            var entity =
+                new Review()
+                {
+                    OwnerId = _userId,
+                    Title = model.Title,
+                    Summary = model.Summary,
+                    ReviewContent = model.ReviewContent,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Review.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
