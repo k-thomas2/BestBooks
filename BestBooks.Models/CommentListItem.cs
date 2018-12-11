8ebb52b4150c195a6ea393a83c4472f38889b6b4
Kate Thomas <kpthomas6@hotmail.com>
2018-12-11 09:51:07 -0500

    updated comment

---
+++ BestBooks.Models/CommentListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBooks.Models
+{
+    public class CommentListItem
+    {
+        public int CommentId { get; set; }
+        public string Content { get; set; }
+        public DateTimeOffset CreatedNote { get; set; }
+
+        public override string ToString() => Content;
+    }
+}
