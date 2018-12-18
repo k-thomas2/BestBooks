8adf7b0d4155b169da82af117f056ce27e977e42
Kate Thomas <kpthomas6@hotmail.com>
2018-12-18 02:23:48 -0500

    updates

---
+++ BestBooks.Services/CommentService.cs
@@ -0,0 +1,57 @@
+﻿using BestBooks.Data;
+using BestBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BestBooks.Data.ApplicationUser;
+
+namespace BestBooks.Services
+{
+    public class CommentService
+    {
+        private readonly Guid _userId;
+
+        public CommentService(Guid userId)
+        {
+            _userId = userId;
+        }
+        //Create Comment 
+        public bool CreateComment(CommentCreate model)
+        {
+            var entity =
+                new Comment()
+                {
+                    OwnerId = _userId,
+                    Content = model.CommentContent,
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Comment.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        //Get Comments 
+        public IEnumerable<CommentListItem> GetComments()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Comment
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                        e =>
+                        new CommentListItem
+                        {
+                            CommentId = e.CommentId,
+                            Content = e.Content
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+
+    }
+}
--- BestBooks_MVC/Controllers/CommentController.cs
+++ BestBooks_MVC/Controllers/CommentController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BestBooks.Models;
+using BestBooks.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +9,48 @@ using System.Web.Mvc;
 
 namespace BestBooks_MVC.Controllers
 {
+    [Authorize]
     public class CommentController : Controller
     {
         // GET: Comment
         public ActionResult Index()
         {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new CommentService(userId);
+            var model = service.GetComments();
+
+            return View(model);
+            
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        //POST: Create 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CommentCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateCommentService();
+
+            if (service.CreateComment(model))
+            {
+                TempData["SaveResult"] = "Your comment was created";
+                return RedirectToAction("Index");
+            };
+            ModelState.AddModelError("", "Comment could not be created.");
+            return View(model);
+        }
+
+        private CommentService CreateCommentService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new CommentService(userId);
+            return service;
         }
     }
 }
