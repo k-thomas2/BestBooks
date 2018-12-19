33e162af1b5b5771149d10d290c1851a480de7fc
Kate Thomas <kpthomas6@hotmail.com>
2018-12-19 11:04:53 -0500

    added method to controller

--- BestBooks.WebApi/Controllers/BookController.cs
+++ BestBooks.WebApi/Controllers/BookController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BestBooks.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +9,20 @@ using System.Web.Http;
 
 namespace BestBooks.WebApi.Controllers
 {
+    [Authorize]
     public class BookController : ApiController
     {
+        public IHttpActionResult Get()
+        {
+            BookService bookService = CreateBookService();
+            var books = bookService.GetBooks();
+            return Ok(books);
+        }
+        private BookService CreateBookService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var bookservice = new BookService(userId);
+            return bookservice;
+        }
     }
 }
