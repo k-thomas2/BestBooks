9b5688403524a417bfc73f5bb8d41d507b68208c
Kate Thomas <kpthomas6@hotmail.com>
2018-12-19 12:01:01 -0500

    added methods to controller

--- BestBooks.WebApi/Controllers/BookController.cs
+++ BestBooks.WebApi/Controllers/BookController.cs
@@ -1,4 +1,5 @@
-﻿using BestBooks.Services;
+﻿using BestBooks.Models;
+using BestBooks.Services;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -12,11 +13,47 @@ namespace BestBooks.WebApi.Controllers
     [Authorize]
     public class BookController : ApiController
     {
-        public IHttpActionResult Get()
+        public IHttpActionResult GetAll()
         {
             BookService bookService = CreateBookService();
             var books = bookService.GetBooks();
             return Ok(books);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            BookService bookService = CreateBookService();
+            var book = bookService.GetBookById(id);
+            return Ok(book);
+        }
+
+        public IHttpActionResult Post(BookCreate book)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateBookService();
+
+            if (!service.CreateBook(book))
+                return InternalServerError();
+            return Ok();
+        }
+
+        public IHttpActionResult Put(BookEdit book)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateBookService();
+            if (!service.UpdateBook(book))
+                return InternalServerError();
+            return Ok();
+        }
+
+        public IHttpActionResult Delete (int id)
+        {
+            var service = CreateBookService();
+            if (!service.DeleteBook(id))
+                return InternalServerError();
+            return Ok();
         }
         private BookService CreateBookService()
         {
